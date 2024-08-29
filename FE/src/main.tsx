fbb51fe46c96e95f8e24b224fbc1268cfa11615e
kimhuePH35955 <148750111+kimhuePH35955@users.noreply.github.com>
2024-08-29 16:17:41 +0700

    first commit

---
+++ FE/src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./global.css";
+import "./styles/style.scss";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+
+);
